2d40ae12adf2f10103203069602c9e7d29929697
Vakindu Philliam <Pvakindu@gmail.com>
2019-09-26 08:58:34 +0300

    Add files via upload

---
+++ framework/src/components/logger/logger.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2019 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const bunyan = require('bunyan');
+
+const createDirIfNotExist = filePath => {
+	const dir = path.dirname(filePath);
+	if (fs.existsSync(dir)) {
+		return;
+	}
+	fs.mkdirSync(dir, { recursive: true });
+};
+
+/**
+ * Creates a logger object with bunyan
+ *
+ * @param {Object} config
+ * @param {string} config.module Name of the module logs are written for
+ * @param {string} config.logFileName
+ * @param {string} config.fileLogLevel
+ * @param {string} config.consoleLogLevel
+ */
+const createLogger = ({
+	fileLogLevel,
+	consoleLogLevel,
+	logFileName,
+	module,
+}) => {
+	const consoleSrc = consoleLogLevel === 'debug' || consoleLogLevel === 'trace';
+	const consoleStream =
+		consoleLogLevel !== 'none'
+			? [
+					{
+						level: consoleLogLevel,
+						stream: process.stdout,
+					},
+			  ]
+			: [];
+	const filePath = path.join(process.cwd(), logFileName);
+	createDirIfNotExist(filePath);
+	const fileSrc = fileLogLevel === 'debug' || fileLogLevel === 'trace';
+	const fileStream =
+		fileLogLevel !== 'none'
+			? [
+					{
+						level: fileLogLevel,
+						path: filePath,
+					},
+			  ]
+			: [];
+	const streams = [...consoleStream, ...fileStream];
+	return bunyan.createLogger({
+		name: 'lisk-framework',
+		streams,
+		src: consoleSrc || fileSrc,
+		serializers: { err: bunyan.stdSerializers.err },
+		module,
+	});
+};
+
+module.exports = {
+	createDirIfNotExist,
+	createLogger,
+};
